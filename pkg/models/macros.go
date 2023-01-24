0708bb459b0248e339cd5c1fa13c5354606742c6
EraldBa <owlyice33@hotmail.com>
2023-01-24 11:34:58 +0200

    Commit

--- pkg/models/macros.go
+++ pkg/models/macros.go
@@ -2,22 +2,30 @@ package models
 
 import (
 	"net/http"
-	"reflect"
 	"strconv"
 )
 
-var macros = map[string]string{
-	"calorie": "Calories",
-	"protein": "Protein",
-	"carbs":   "Carbs",
-	"fats":    "Fats",
+var macros = [4]string{
+	"calorie",
+	"protein",
+	"carbs",
+	"fats",
 }
 
 // SetMacros takes strings from form data, converts them to floats and sets the macros of StatsGet
 func (s *StatsGet) SetMacros(r *http.Request) {
-	statsVal := reflect.ValueOf(s).Elem()
-	for formField, structField := range macros {
+	for _, formField := range macros {
 		value, _ := strconv.ParseFloat(r.Form.Get(formField), 32)
-		statsVal.FieldByName(structField).SetFloat(value)
+		value32 := float32(value)
+		switch formField {
+		case "calorie":
+			s.Calories = value32
+		case "protein":
+			s.Protein = value32
+		case "carbs":
+			s.Carbs = value32
+		case "fats":
+			s.Fats = value32
+		}
 	}
 }
